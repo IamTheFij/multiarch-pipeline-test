b5364e3bf9b05c89dfda132c226ff5cdb1378439
Ian Fijolek <ian@iamthefij.com>
2019-06-04 17:33:15 -0700

    Debug working dir

--- get_qemu.sh
+++ get_qemu.sh
@@ -8,7 +8,7 @@ cd build
 
 # Multiple args can be passed in, but in most cases (Makefile and .drone.yml) we only use one at a time
 for target_arch in $*; do
-  wget -N https://github.com/multiarch/qemu-user-static/releases/download/$VERSION/${HOST_ARCH}_qemu-${target_arch}-static.tar.gz
+  wget https://github.com/multiarch/qemu-user-static/releases/download/$VERSION/${HOST_ARCH}_qemu-${target_arch}-static.tar.gz
   tar -xvf ${HOST_ARCH}_qemu-${target_arch}-static.tar.gz
   rm ${HOST_ARCH}_qemu-${target_arch}-static.tar.gz
 done
